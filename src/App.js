6c47830ea3450ffada0a1c59c8379b03017f790a
jooprima <jooprima@gmail.com>
2021-06-28 14:34:33 +0700

    create empty component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 import Navbar from "./components/Navbar";
 import Container from "./components/Container";
+import Empty from "./components/Empty";
 
 function App() {
   const [emojiData, setEmojiData] = useState([]);
@@ -38,8 +39,8 @@ function App() {
       <Container>
         <h1>Hello, world!</h1>
 
-        {loading && <p>Loading...</p>}
-        {error && <p>Ooopppss...</p>}
+        {loading && <Empty text="Loading..." />}
+        {error && <Empty text="Error!" />}
         {emojiData.length > 0 && <p>Emojis Data Sukses!</p>}
       </Container>
     </>
